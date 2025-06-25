b0ca780d63c8bf8494c1b51ee5493ab6cc413b28
Cassio <cassioschardosim525@gmail.com>
2025-06-25 18:23:51 -0300

    Add files via upload

---
+++ Jogo/ia.py
@@ -0,0 +1,7 @@
+for a in range(0,2):
+    for b in range(0,2):
+        for c in range(0,2):
+            for d in range(0,2):
+                for g in range(0,2):
+                   for f in range(0,2):
+                       print(f)
