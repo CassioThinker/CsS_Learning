9c434d6d9dfcb1ca52a19de185fbc6a590804c2f
Cassio <cassioschardosim525@gmail.com>
2025-06-25 18:25:54 -0300

    Delete Jogo directory

--- Jogo/ia.py
+++
@@ -1,7 +0,0 @@
-for a in range(0,2):
-    for b in range(0,2):
-        for c in range(0,2):
-            for d in range(0,2):
-                for g in range(0,2):
-                   for f in range(0,2):
-                       print(f)
